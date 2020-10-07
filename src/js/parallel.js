d2e082dd210b1e032c93a25f67f39d282911c60c
Billy Porter <wporter2@nd.edu>
2020-10-06 23:29:22 -0400

    New folder structure

---
+++ src/js/parallel.js
@@ -0,0 +1,11 @@
+/**
+ * Returns an object of the following form:
+ * [
+ *      { id, exam1, exam2, exam3, finalExam },
+ *      ...,
+ *      { id, exam1, exam2, exam3, finalExam }
+ * ]
+ */
+function formatParallelData() {
+    console.log("PC");
+}
---
+++ src/js/sankey.js
@@ -0,0 +1,20 @@
+/**
+ * Returns data of the following form:
+ * {
+ *      "nodes": [
+ *          {"node": 0, "name": "A"},
+ *          {"node": 1, "name": "B"},
+ *          ...
+ *      ]
+ *      "links": [
+ *          {"source": <FIRSTASSIGN_LETTER>, "target": <SECONDASSIGN_LETTEr>, "value": <NUMBER_OF_STUDENTS_FROM_FIRST_TO_SECOND}
+ *      ]
+ * }
+ * 
+ * IMPORTANT: Will need a letter grade for each assignment, so there should be
+ * (number of assignments) * (number of letters) = totalNodes
+ * 
+ */
+function formatSankeyData() {
+    console.log('Sankey');
+}
