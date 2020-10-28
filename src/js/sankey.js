24c3438b30c0989b7bc38bd77a1752f06bbfb01a
Conor Murphy <conor.p.murphy52@gmail.com>
2020-10-28 16:55:55 -0400

    first draft of sankey

--- src/js/sankey.js
+++ src/js/sankey.js
@@ -18,6 +18,12 @@
 const assessments = ["Exam 1", "Exam 2", "Exam 3", " Final Exam", "Calculated Grade"];
 const grades = ["A", "B", "C", "D", "F"];
 
+const width = 740;
+const height = 500;
+const svgBackground = "#eff";
+const svgBorder = "1px solid #333";
+const margin = 10;
+
 function gradeScale(score){
     if(!score){
         return "";
@@ -134,10 +140,47 @@ function formatSankeyData(data) {
 
 const data = formatSankeyData(rawData);
 console.log(data);
+const svg = d3.select("#canvas")
+                  .attr("width", width)
+                  .attr("height", height)
+                  .style("background-color", svgBackground)
+                  .style("border", svgBorder)
+                  .append("g")
+                  .attr("transform", `translate(${margin},${margin})`);
+                  const graphSize = [width - 2*margin, height - 2*margin];
 const sankey = d3.sankey()
-    .size([500, 500])
+    .size([width, height])
     .nodeId(d => d.id)
     .nodeWidth(20)
     .nodePadding(10)
     .nodeAlign(d3.sankeyCenter);
 let graph = sankey(data);
+
+let graphlink = svg
+    .append("g")
+    .classed("links", true)
+    .selectAll("path")
+    .data(graph.links)
+    .enter()
+    .append("path")
+    .classed("link", true)
+    .attr("d", d3.sankeyLinkHorizontal())
+    .attr("fill", "none")
+    .attr("stroke", "#606060")
+    .attr("stroke-width", d => d.width)
+    .attr("stoke-opacity", 0.5);
+
+let graphnode = svg
+    .append("g")
+    .classed("nodes", true)
+    .selectAll("rect")
+    .data(graph.nodes)
+    .enter()
+    .append("rect")
+    .classed("node", true)
+    .attr("x", d => d.x0)
+    .attr("y", d => d.y0)
+    .attr("width", d => d.x1 - d.x0)
+    .attr("height", d => d.y1 - d.y0)
+    .attr("fill", "blue")
+    .attr("opacity", 0.8);
