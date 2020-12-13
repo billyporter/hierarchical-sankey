1a42ef2d24bced669881ec916ce30dfb1927a0d6
Billy Porter <wporter2@nd.edu>
2020-12-13 12:02:41 -0500

    Bar graph: missing labels

--- src/js/legend.js
+++ src/js/legend.js
@@ -6,22 +6,22 @@
 function buildLegend(colorArray, rankedArray) {
 
     const legendX = width + legendWidth / 2;
-    clearPrevLegend();
-    for (let i = 0; i < rankedArray.length && i < 8; i++) {
-        svg.append("circle")
-            .attr("cx", legendX)
-            .attr("cy", 130 + 30 * i)
-            .attr("r", 6)
-            .style("fill", colorArray[i])
-            .attr("class", "leg");
-        svg.append("text")
-            .attr("x", legendX + 30)
-            .attr("y", 130 + 30 * i)
-            .text(rankedArray[i][0] + " " + rankedArray[i][1])
-            .style("font-size", "15px")
-            .attr("alignment-baseline", "middle")
-            .attr("class", "leg");
-    }
+    // clearPrevLegend();
+    // for (let i = 0; i < rankedArray.length && i < 8; i++) {
+    //     svg.append("circle")
+    //         .attr("cx", legendX)
+    //         .attr("cy", 130 + 30 * i)
+    //         .attr("r", 6)
+    //         .style("fill", colorArray[i])
+    //         .attr("class", "leg");
+    //     svg.append("text")
+    //         .attr("x", legendX + 30)
+    //         .attr("y", 130 + 30 * i)
+    //         .text(rankedArray[i][0] + " " + rankedArray[i][1])
+    //         .style("font-size", "15px")
+    //         .attr("alignment-baseline", "middle")
+    //         .attr("class", "leg");
+    // }
     const barData = buildBarGraphData(rankedArray, colorArray);
     console.log(barData);
 }
@@ -39,21 +39,26 @@ function clearPrevLegend() {
 function buildBarGraphData(rankedArray, colorArray) {
     d3.selectAll(".bar").remove();
     barData = []
+    let i = 0;
     for (let group of rankedArray) {
         barData.push({ "Exam": group[0], "Students": group[1] });
+        i += 1;
+        if (i == 8) {
+            break;
+        }
     }
 
-    const numBars = rankedArray.length;
-    const barHeight = 150;
-    const barPadding = 10;
-    const barWidth = barHeight / numBars;
+    const numBars = rankedArray.length < 8 ? rankedArray.length : 8;
+    const barHeight = 200;
+    const barPadding = 15;
+    const barWidth = 30;
 
     var bar_x = d3.scaleLinear()
         .range([0, 145])
         .domain([0, 200]);
 
     domainY = []
-    for (let i = 0; i < numBars; i++) {
+    for (let i = 0; i < 9; i++) {
         domainY.push(i);
     }
 
@@ -67,7 +72,9 @@ function buildBarGraphData(rankedArray, colorArray) {
         .append("rect")
         .attr("class", "bar")
         .attr("y", function (d, i) {
-            return bar_y(i);
+            console.log(d, i);
+            console.log(bar_y(i));
+            return bar_y(i) + 150;
         })
         .attr("height", barWidth - barPadding)
         .attr("x", 800)
