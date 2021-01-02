c66e3b1f9eeeaea889f2d93c9eb38774e6c9aadd
Dane Williams <dwilli36@nd.edu>
2021-01-01 21:33:04 -0800

    implements coloring functions in links

--- src/js/sankey.js
+++ src/js/sankey.js
@@ -135,11 +135,16 @@ function drawLinks(graph) {
         .attr("fill", "none")
         .style("stroke-width", d => d.width)
         .style("stroke", d => {
-            /* If a letter */
-            if (letrs.has(d.source.name[0])) {
-                return sankeyColor(d.source.name[0])
-            }
-            return sankeyColor(gradeScale(d.source.name));
+            /* case for whole letter grade nodes */
+            if (letrs.has(d.source.name))
+                return sankeyColor(d.source.name);
+
+            /* case for + and - grade nodes */
+            if (letrs.has(d.source.name[0]))
+                return getShadePlusMinus(sankeyColor(d.source.name[0]), d.source.name[1]);
+
+            /* case for number grade nodes */ 
+            return getShadeNumber(sankeyColor(gradeScale(d.source.name)), d.source.name);
         })
         .on("mouseover", (d, i) => {
             if (!isActive) {
