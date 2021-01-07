2c7a4869937c680392780cd8701137d8a25b6b1f
Billy Porter <wporter2@nd.edu>
2021-01-06 20:44:36 -0500

    Pc improvements

--- src/js/sankey.js
+++ src/js/sankey.js
@@ -45,8 +45,8 @@ function drawSankey(sankeyData, isFirst, isBreakdown, oldData, brokeExam, brokeG
         populatePointStorageObj(graph);
         populateLinkStorageObj(graph);
         drawNodes(graph);
-        drawPC(sankeyData);
         drawLinks(graph);
+        drawPC(sankeyData);
     }
 
 
@@ -204,16 +204,17 @@ function drawLinks(graph) {
         .enter()
         .append("path")
 
-    document.addEventListener("click", function (event) {
+    document.addEventListener("click", function (event, d) {
         const target = event.target;
         if (!target.closest('.link')) {
             isActive = false;
             d3.selectAll(".lines")
                 .style("visibility", "hidden")
-            d3.selectAll(".link").style('pointer-events', 'auto');
+            d3.selectAll(".link").style('pointer-events', 'auto').style('stroke-opacity', "0.4");
             d3.selectAll(".axes")
                 .style("visibility", "hidden");
             clearPrevLegend();
+            d3.selectAll(".lines").style('pointer-events', 'none');
         }
     })
 
@@ -230,22 +231,25 @@ function drawLinks(graph) {
             if (!isActive) {
                 hoverBehavior(i, false);
                 d3.selectAll(".axes").style("visibility", "visible");
+                highlightLink(i, false);
             }
         })
         .on("click", function (d, i) {
             isActive = true
             activeLink = i.index;
             hoverBehavior(i, true);
-            d3.selectAll(".link").style('pointer-events', 'none');
+            highlightLink(i, true);
             d3.selectAll(".axes").style("visibility", "visible");
+            d3.selectAll(".lines").style('pointer-events', 'visiblePainted');
+            d3.selectAll(".link").style('pointer-events', 'none');
         })
         .on("mouseout", () => {
             if (!isActive) {
                 d3.selectAll(".lines").style("visibility", "hidden");
                 d3.selectAll(".axes").style("visibility", "hidden");
+                d3.selectAll(".link").style("stroke-opacity", "0.4");
             }
         });
-
 }
 
 /**
@@ -381,8 +385,8 @@ function transitionToNewBreakdown(sankeyData, newPointsNotInOldSet, oldPointsNot
         if (soFar === total) {
             removePlots();
             drawNodes(graph);
-            drawPC(sankeyData);
             drawLinks(graph);
+            drawPC(sankeyData);
         }
     });
 }
@@ -510,8 +514,8 @@ function transitionToNewBuildup(newPointsNotInOldSet, oldPointsNotInNewSet, oldL
         if (soFar === total) {
             removePlots();
             drawNodes(graph);
-            drawPC(sankeyData);
             drawLinks(graph);
+            drawPC(sankeyData);
         }
     });
 }
